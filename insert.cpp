74e3060808b01ee35621178f5ea54baa69875388
capminhhoang60135638 <hoang.cm.60cntt@ntu.edu.vn>
2020-12-31 15:29:15 +0700

    Thay doi du lieu dau vao va tinh gioi gian chay code

--- insert.cpp
+++ insert.cpp
@@ -39,14 +39,27 @@ void printArray(int arr[], int n)
 /* Driver code */
 int main()  
 {  
-    int arr[] = { 5, 10, 15, 20, 25, 50, 40, 30, 20, 10, 9524, 878, 17, 1, 99, 18785, 3649, 164, 94,
-		123, 432, 654, 3123, 654, 2123, 543, 131, 653, 123, 533, 1141, 532, 213, 2241, 824, 1124, 42, 134, 411,
-		491, 341, 1234, 527, 388, 245, 1992, 654, 243, 987 };  
-    int n = sizeof(arr) / sizeof(arr[0]);  
-    clock_t start = clock();
+    int arr[500];
+    int i=0;
+    FILE *f =fopen("D:\\Thuctapcoso\\ThucTapCoSo\\dauvao500.txt","r");
+    if (f == NULL)
+	{
+		cout<<"Can't open file!";
+	} 
+    else{
+        int so;
+        while (fscanf(f, "%d", &so) != EOF)
+	        {
+                arr[i]=so;
+                i++;
+	        }
+    }
+     int n = sizeof(arr) / sizeof(arr[0]);
+    clock_t begin = clock();
     insertionSort(arr, n);  
-     
-     printf("Time: %.9fs\n", (double)(clock() - start)/CLOCKS_PER_SEC);
+     clock_t end = clock();
+	  // cout<<"Time run: "<<(float)(/*end-*/begin)/*/CLOCKS_PER_SEC*/<<" s"<<endl;
+      cout<<begin<<"\n"<<end<<"\n";
      printArray(arr, n); 
      getchar();
     return 0;  
