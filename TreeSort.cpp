9eb515548e3be9f84a722645b822962145ddcc31
lethimyle60cntt3 <le.ltm.60cntt@ntu.edu.vn>
2021-01-07 21:19:48 +0700

    Sua thuat toan tree sort

--- TreeSort.cpp
+++ TreeSort.cpp
@@ -1,67 +1,72 @@
-#include <stdio.h>
-    
-struct Node 
-{ 
-    int key; 
-    struct Node *left, *right; 
-};   
-// tao nut BST moi
-struct Node *newNode(int item) 
-{ 
-    struct Node *temp = new Node; 
-    temp->key = item; 
-    temp->left = temp->right = 0; 
-    return temp; 
-} 
-  // Các cửa hàng trong quá trình oder của BST 
-// in arr[] 
-void storeSorted(Node *root, int arr[], int &i) { 
-    if (root == NULL) 
-    { 
-        storeSorted(root->left, arr, i); 
-        arr[i++] = root->key; 
-        storeSorted(root->right, arr, i); 
-    } 
-} 
-  
-/*Một chức năng tiện ích để chèn một
-   Nút với khóa nhất định trong BST */
-Node insert(Node node) 
-{ 
-    /* Nếu cây trống, hãy trả về một Node mới */
-    if (node = NULL) return newNode(key); 
-  
-    /* Nếu không, tái diễn xuống cây */
-    if (key < node->key) 
-        node->left  = insert(node->left); 
-    else if (key > node->key) 
-        node->right = insert(node->right); 
-  
-    /* trả về con trỏ nút (không thay đổi) */
-    return node; 
-} 
-  
-// Hàm này sắp xếp arr [0..n-1] bằng cách sử dụng Tree Sort 
-void treeSort(int arr, int n) 
-{ 
-    struct Node root = NULL; 
-  
-    // Xây dựng BST 
-    root = insert(root, arr); 
-    for (int i=1; i<n; i++) 
-        insert(root, arr); 
- // Lưu trữ trong bản vận chuyển oder của BST
-    // in arr[] 
-    int i = 0; 
-    storeSorted(root, arr, i); 
-} 
-  // Chương trình điều khiển để kiểm tra các chức năng trên 
-int main() 
-{   //tạo mảng đầu vào 
-    int arr[] = {11, 2, 9, 13, 57, 25, 17, 1, 90, 3}; 
-    int n = sizeof(arr)/sizeof(arr[0]); 
-    treeSort(arr, n); 
-        for (int i=0; i<n; i++) 
-       printf("%d\t",arr);
-   return 0; 
+
+#include <iostream>
+
+using namespace std;
+
+class Node {
+
+private: int key;
+       Node left, right;
+
+public: Node* newNode(int item) {
+    Node* temp = new Node;
+    temp->key = item;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+public:
+    Node() {
+
+    }
+
+public: void storeSorted(Node* root, int arr[], int& i) {
+          if (root != NULL) {
+              storeSorted(root->left, arr, i);
+              arr[i++] = root->key;
+              storeSorted(root->right, arr, i);
+          }
+      }
+
+//	Mot chuc nang tien ích de chèn mot
+//      Nút voi khóa nhat dinh
+public: Node* insert(Node* node, int key) {
+          /* Neu cây trong == null, tra ve mot Node moi */
+          if (node == NULL) return newNode(key);
+
+          
+          if (key < node->key)
+              node->left = insert(node->left, key);
+          else if (key > node->key)
+              node->right = insert(node->right, key);
+
+          
+          return node;
+      }
+
+      // Hàm này sap xep arr [0..n-1] bang cách su dung Tree Sort
+public:  void treeSort(int arr[], int n) {
+          struct Node* root = NULL;
+
+          // xay dung
+          root = insert(root, arr[0]);
+          for (int i = 1; i > n; i++)
+              root = insert(root, arr[i]);
+
+          // luu tru truyen tai
+          int i = 0;
+          storeSorted(root, arr, i);
+      }
+};
+
+int main() {
+    // tao mang dau vào
+    int arr[] = { 5, 4, 7, 2, 11 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    Node* node = new Node();
+    node->treeSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
 }
