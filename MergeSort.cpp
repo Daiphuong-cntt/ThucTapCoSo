7854882b487be08b1086e961b02e651927a26c0f
Võ Đình Chí <chi.vd.60cntt@ntu.edu.vn>
2021-01-08 09:25:15 +0700

    code Merge Sort theo huong doi tuong

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,101 +1,89 @@
-#include<stdio.h>
-
-void Nhapdl(int A[],int &n)
-{
-	printf("Nhap So luong pt cua mang: ");
-	scanf("%d",&n);
-	
-	for(int i=0;i<n;i++)
-	{
-		printf("A[%d]: ",i+1);
-		scanf("%d",&A[i]);
-	}
-}
-void Xuatdl(int A[],int n)
-{
-	for(int i=0;i<n;i++)
-	{
-		printf("%3d",A[i]);
-	}
-}
-
-void Merge(int A[],int l,int r,int m)
-{
-	// kich thuoc 2 mang con
-	int n1=m+1-l;
-	int n2=r-m;
-	
-	// tao 2 mang con
-	int L[n1];
-	int R[n2];
-	
-	// sao chep dl vao 2 mang
-	for(int i=0;i<n1;i++)
-	{
-		L[i]=A[l+i];
-	}
-	for(int j=0;j<n2;j++)
-	{
-		R[j]=A[m+1+j];
+#include <iostream>
+using namespace std;
+ 
+class MergeSort{
+	public: MergeSort(){
+		
 	}
 	
-	// gop 2 mang con
-	int i=0;
-	int j=0;
-	int k=l;
-	while(i<n1 && j<n2)
+	public: void merge(int arr[], int l, int m, int r)
 	{
-		if(L[i]<=R[j])
-		{
-			A[k]=L[i];
-			i++;
-		}
-		else
-		{
-			A[k]=R[j];
-			j++;
-		}
-		k++;
-	}
+	    int n1 = m - l + 1;
+	    int n2 = r - m;
+	 
+	 
+	    int L[n1], R[n2];
+	 
+	   
+	    for (int i = 0; i < n1; i++)
+	        L[i] = arr[l + i];
+	    for (int j = 0; j < n2; j++)
+	        R[j] = arr[m + 1 + j];
 	
-	// sap xep cac phan tu con lai vao mang
-	while(i<n1)	// mang trai L
-	{
-		A[k]=L[i];
-		i++;
-		k++;
+	 
+	    int i = 0;
+	 
+	    
+	    int j = 0;
+	 
+	    
+	    int k = l;
+	 
+	    while (i < n1 && j < n2) {
+	        if (L[i] <= R[j]) {
+	            arr[k] = L[i];
+	            i++;
+	        }
+	        else {
+	            arr[k] = R[j];
+	            j++;
+	        }
+	        k++;
+	    }
+	 
+	  
+	    while (i < n1) {
+	        arr[k] = L[i];
+	        i++;
+	        k++;
+	    }
+	 
+	   
+	    while (j < n2) {
+	        arr[k] = R[j];
+	        j++;
+	        k++;
+	    }
 	}
-	while(j<n2)	// mang phai R
-	{
-		A[k]=R[j];
-		j++;
-		k++;
+	 
+	
+	public: void mergeSort(int arr[],int l,int r){
+	    if(l>=r){
+	        return;
+	    }
+	    int m = (l+r-1)/2;
+	    mergeSort(arr,l,m);
+	    mergeSort(arr,m+1,r);
+	    merge(arr,l,m,r);
 	}
-}
-
-// Ham tach mang va sap xep lai
-void MergeSort(int A[],int l,int r)
-{
-	if(l<r)
+	 
+	
+	public: void Xuatmang(int A[], int size)
 	{
-		int m=(l+r)/2;
-		// ham de qui de tach mang
-		MergeSort(A,l,m);
-		MergeSort(A,m+1,r);
-		// Tron 2 mang
-		Merge(A,l,r,m);
+	    for (int i = 0; i < size; i++)
+	        cout << A[i] << " ";
 	}
-}
+	 
+};
 int main()
 {
-	int A[100];
-	int n;
-	Nhapdl(A,n);
-	printf("Mang vua nhap:");
-	Xuatdl(A,n);
-	
-	MergeSort(A,0,n-1);
-	printf("\nChuoi sau sap xep:");
-	Xuatdl(A,n);
-	return 0;
+    int arr[] = {23, 1, 32, 68, 89, 200, 55, 78, 99, 12};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+ 
+ 	MergeSort* node =new MergeSort();
+    node->mergeSort(arr, 0, arr_size - 1);
+
+    cout << "Mang sau sap xep \n";
+    node->Xuatmang(arr, arr_size);
+    return 0;
 }
