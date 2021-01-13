df97dec49d99511f36efbc624449765db263818b
Daiphuong-cntt <63713633+Daiphuong-cntt@users.noreply.github.com>
2021-01-14 06:09:11 +0700

    Đếm thời gian

--- Program.cs
+++ Program.cs
@@ -6,8 +6,20 @@ namespace ThucTapCoSo
     {
         static void Main(string[] args)
         {
+            DateTime startdate = DateTime.Now;
             CoutingSort.SortArrayWithCountingSort();
-           TimSort.SortArrayWithTimSort();
+            DateTime finishDate = DateTime.Now;
+            TimeSpan time = finishDate - startdate;
+            Console.WriteLine("Start:=" + startdate.ToString("dd-MM-yyyy HH:mm:ss.fffffff"));
+            Console.WriteLine("Finish:=" + finishDate.ToString("dd-MM-yyyy HH:mm:ss.fffffff"));
+            Console.WriteLine("Total:=" + time.Ticks);
+           DateTime startdate = DateTime.Now;
+            TimSort.SortArrayWithTimSort();
+            DateTime finishDate = DateTime.Now;
+            TimeSpan time = finishDate - startdate;
+            Console.WriteLine("Start:=" + startdate.ToString("dd-MM-yyyy HH:mm:ss.fffffff"));
+            Console.WriteLine("Finish:=" + finishDate.ToString("dd-MM-yyyy HH:mm:ss.fffffff"));
+            Console.WriteLine("Total:=" + time.Ticks);
 
 
 
