febf0ff1a49d42bdccd1756f3dde87c4232c6dc5
Daiphuong-cntt <63713633+Daiphuong-cntt@users.noreply.github.com>
2021-01-18 02:04:36 +0700

    Thêm ghi chú

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace ThucTapCoSo
     {
         static void Main(string[] args)
         {
+            //Đếm thời gian cho Counting Sort
             DateTime startdate = DateTime.Now;
             CoutingSort.SortArrayWithCountingSort();
             DateTime finishDate = DateTime.Now;
@@ -13,7 +14,9 @@ namespace ThucTapCoSo
             Console.WriteLine("Start:=" + startdate.ToString("dd-MM-yyyy HH:mm:ss.fffffff"));
             Console.WriteLine("Finish:=" + finishDate.ToString("dd-MM-yyyy HH:mm:ss.fffffff"));
             Console.WriteLine("Total:=" + time.Ticks);
-           DateTime startdate = DateTime.Now;
+            Console.WriteLine();
+             //Đếm thời gian cho Timsort
+            DateTime startdate = DateTime.Now;
             TimSort.SortArrayWithTimSort();
             DateTime finishDate = DateTime.Now;
             TimeSpan time = finishDate - startdate;
