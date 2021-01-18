c9090a42d4135d9ec66aae200d3dbf1de0dd2763
Võ Đình Chí <chi.vd.60cntt@ntu.edu.vn>
2021-01-18 15:36:21 +0700

    Cap nhat code

--- CodeChinh.cpp
+++ CodeChinh.cpp
@@ -1,158 +1,127 @@
-#include<iostream>
-#include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
-#include<fstream> 
+#include <iostream>
+#include <fstream>
+#include <ctime>
 
 using namespace std;
 
-class Source{
+class Source
+{
 	public:
-	Source(){}
-	void CreSource(int A[],int n)
+    Source()
 	{
-		srand(time(NULL));
-		for(int i=0;i<n;i++)
-		{
-			A[i]=rand();
-		}
+		
 	}
-	void PrSource(int A[],int n)
-	{
-		for(int i=0;i<n;i++)
-		{
-			cout << A[i] << " ";
-		}
-	}
-};
-class Source_100: public Source{
-	public:
-	int dauvao100[100];
-	int n=100;
-	Source_100(): Source(){}
-	void DocFile()
-	{
-		fstream fsFile;
-		fsFile.open("D:\\dauvao100.txt", ios::in);
-	    if (fsFile.eof())
-	    {
-	        cout << "Doc file thanh cong !";
-	    }
+    void DocFile(char* s,int arr[])
+    {
+        fstream F;
+        F.open(s, ios::in);
+        if (F.eof())
+        {
+            cout << "Doc file thanh cong !";
+        }
         else
-	    {
-	  		int tam ;
-	        int i = 0;
-	        while (fsFile>>tam)
-	        {
-	            this->dauvao100[i]=tam;
-	            i++;
-	        }
-	    }
-	}
-	void TaoMang_100(int dauvao100[],int n)
-	{
-		Source::CreSource(dauvao100,n);
-	}
-	void XuatMang_100(int dauvao100[],int n)
-	{
-		Source::PrSource(dauvao100,n);
-	}
+        {
+  		    int tam ;
+            int i = 0;
+            while (F >> tam)
+            {
+                arr[i] = tam;
+                i++;
+            }
+        }
+        F.close();
+    }
+    void XuatMang(int arr[],int n)
+    {
+    	cout << "\nMang chua sap xep:\n";
+        for(int i = 0 ; i < n ; i++)
+        {
+            cout << arr[i] << " ";
+        }
+    }
 };
-class Source_250: public Source{
+class Source100: public Source
+{
 	public:
-	int dauvao250[250];
-	int n=250;
-	Source_250(): Source(){}
-	void DocFile()
+	int dulieu100[100];
+	int n = 100;
+	Source100(): Source()
 	{
-		fstream fsFile;
-		fsFile.open("D:\\dauvao2500.txt", ios::in);
-	    if (fsFile.eof())
-	    {
-	        cout << "Doc file thanh cong !";
-	    }
-        else
-	    {
-	  		int tam ;
-	        int i = 0;
-	        while (fsFile>>tam)
-	        {
-	            this->dauvao250[i]=tam;
-	            i++;
-	        }
-	    }
+	
 	}
-	void TaoMang_250(int dauvao250[],int n)
+	void LayMangFile100(char *s, int dulieu100[])
 	{
-		Source::CreSource(dauvao250,n);
+		Source::DocFile(s,dulieu100);	
 	}
-	void XuatMang_250(int dauvao250[],int n)
+	void XuatMang100(int dulieu100[],int n)
 	{
-		Source::PrSource(dauvao250,n);
+		Source::XuatMang(dulieu100,n);
 	}
 };
-class Source_500: public Source{
+class Source250: public Source
+{
 	public:
-	int dauvao500[500];
-	int n=500;
-	Source_500(): Source(){}
-	void DocFile()
+	int dulieu250[250];
+	int n = 250;
+	Source250(): Source()
 	{
-		fstream fsFile;
-		fsFile.open("D:\\dauvao500.txt", ios::in);
-	    if (fsFile.eof())
-	    {
-	        cout << "Doc file thanh cong !";
-	    }
-        else
-	    {
-	  		int tam ;
-	        int i = 0;
-	        while (fsFile>>tam)
-	        {
-	            this->dauvao500[i]=tam;
-	            i++;
-	        }
-	    }
+	
 	}
-	void TaoMang_500(int dauvao500[],int n)
+	void LayMangFile250(char *s, int dulieu250[])
 	{
-		Source::CreSource(dauvao500,n);
+		Source::DocFile(s,dulieu250);	
 	}
-	void XuatMang_500(int dauvao500[],int n)
+	void XuatMang250(int dulieu250[],int n)
 	{
-		Source::PrSource(dauvao500,n);
+		Source::XuatMang(dulieu250,n);
 	}
 };
-
-// thuat toan sap xep
+class Source500: public Source
+{
+	public:
+	int dulieu500[500];
+	int n = 500;
+	Source500(): Source()
+	{
+	
+	}
+	void LayMangFile500(char *s, int dulieu500[])
+	{
+		Source::DocFile(s,dulieu500);	
+	}
+	void XuatMang500(int dulieu500[],int n)
+	{
+		Source::XuatMang(dulieu500,n);
+	}
+};
+/*====================================================================*/
 class Sort{
-	public:
-	void printArray(int A[],int n)
-	{
-		for(int i=0;i<n;i++)
-		{
-			cout << A[i] << " ";
-		}
-	}
+	public: void inmang(int arr[],int n)
+    {
+    	cout << "\nMang sau sap xep\n";
+        for (int i = 0 ; i < n ; i++) 
+        cout << arr[i] << " "; 
+    }
 };
-class MergeSort: public Sort{	
+class MergeSort: public Sort
+{	
 	public:
 	void merge(int arr[], int l, int m, int r)
 	{
-		// tao 2 mang con.
+		// tao 2 mang con
 	    int n1 = m - l + 1;
 	    int n2 = r - m;
 	    int L[n1], R[n2];
-	    // chep du lieu vao 2 mang
+	    //  sao chep du lieu vao 2 mang
 	    for (int i = 0; i < n1; i++)
 	        L[i] = arr[l + i];
 	    for (int j = 0; j < n2; j++)
 	        R[j] = arr[m + 1 + j];
+	        
+	    int i = 0;	// khoi tao chi so bat dau cua mang L
+		int j = 0;	// khoi tao chi so bat dau cua mang R
+		int k = l;	// khoi tao chi so bat dau cua mang luu ket qua
 	    // thuc hien tron code
-	    int i = 0;
-		int j = 0;
-		int k = l;
 	    while (i < n1 && j < n2)
 		{
 	        if (L[i] <= R[j]) {
@@ -165,44 +134,52 @@ class MergeSort: public Sort{
 	        }
 	        k++;
 	    }
-	    // sao chep cac phan tu con lai vao mang (neu co)
-	    while (i < n1) {
+	    // sao chep cac phan tu con lai vao mang neu co
+	    while (i < n1)
+		{
 	        arr[k] = L[i];
 	        i++;
 	        k++;
 	    }
-	    while (j < n2) {
+	    while (j < n2)
+		{
 	        arr[k] = R[j];
 	        j++;
 	        k++;
 	    }
 	}
 	// Ham de qui sap xep chinh
-	void mergeSort(int arr[],int l,int r)
+	void mergeSort(int arr[], int l, int r)
 	{
 	    if(l<r)
 		{	
-			int m = (l+r)/2;
+			int m = ( l + r ) / 2;
 			// de qui tach mang
-		    mergeSort(arr,l,m);
-		    mergeSort(arr,m+1,r);
+		    mergeSort(arr, l, m);
+		    mergeSort(arr, m+1, r);
 		    // tron code 
-		    merge(arr,l,m,r);
+		    merge(arr, l, m, r);
 	    }
 	}
-	double ThoiGianThucHien(int arr[],int n)
+	void InMang(int arr[], int n)
 	{
-		clock_t star = (double)clock();
-		int l=0;
-		int r=n-1;
-		mergeSort(arr,l,r);
-		//Sort:: printArray(arr,n);
+		cout << "\nMerge Sort:\n";
+		Sort::inmang(arr, n);
+	}
+	// tinh thoi gian thuc hien thuat toan MergeSort
+	double t_MergeSort (int arr[], int n)
+	{
+		int l = 0;	// arr bat dau tai l
+		int r = n - 1; // ket thuc tai r
+		clock_t star = clock();
+			mergeSort(arr, l, r);
 		clock_t end = clock();
-		double d = (double)(end-star)/CLOCKS_PER_SEC;
+		double d = (double)(end - star) / CLOCKS_PER_SEC;
 		return d;
 	}
 };
-class HeapSort : public Sort{
+class HeapSort : public Sort
+{
 	public:
     // tim max heap
 	void heapify(int arr[], int n, int i)
@@ -238,33 +215,42 @@ class HeapSort : public Sort{
 			heapify(arr, i, 0);
 		}
 	}
-	double ThoiGianThucHien(int arr[],int n)
+	void InMang (int arr[], int n)
+	{
+		cout << "\nHeap Sort:\n";
+		Sort::inmang(arr, n);
+	}
+	// tinh thoi gian thuc hien thuat toan HeapSort
+	double t_HeapSort(int arr[], int n)
 	{
 		clock_t star = clock();
-		heapSort(arr,n);
-		//Sort:: printArray(arr,n);
+			heapSort(arr, n);
 		clock_t end = clock();
-		double d = (double)(end-star)/CLOCKS_PER_SEC;
+		double d = (double)(end - star) / CLOCKS_PER_SEC;
 		return d;
 	}
 };
 int main()
 {
-    MergeSort ms100, ms250, ms500;
-    HeapSort hs100, hs250, hs500;
- 	
- 	Source_100 s100;
-	s100.DocFile();	
-	s100.TaoMang_100(s100.dauvao100,s100.n);
- 	Source_250 s250;
-	s250.DocFile();	
-	s250.TaoMang_250(s250.dauvao250,s250.n);
- 	Source_500 s500;
-	s500.DocFile();	
-	s500.TaoMang_500(s500.dauvao500,s500.n);
-    
-    cout << "Size\t\t100\t250\t500";
-    cout << "\nMergeSort\t" << ms100.ThoiGianThucHien(s100.dauvao100,s100.n) << "\t" << ms250.ThoiGianThucHien(s250.dauvao250,s250.n) << "\t" << ms500.ThoiGianThucHien(s500.dauvao500,s500.n);
-	cout << "\nHeapSort\t"	<< hs100.ThoiGianThucHien(s100.dauvao100,s100.n) << "\t" << hs250.ThoiGianThucHien(s250.dauvao250,s250.n) << "\t" << hs500.ThoiGianThucHien(s500.dauvao500,s500.n);
-	return 0;
+	Source100 s100_1, s100_2;
+	s100_1.LayMangFile100("D:\\dauvao100.txt", s100_1.dulieu100);
+	s100_2.LayMangFile100("D:\\dauvao100.txt", s100_2.dulieu100);
+	
+	Source250 s250_1, s250_2;
+	s250_1.LayMangFile250("D:\\dauvao250.txt", s250_1.dulieu250);
+	s250_2.LayMangFile250("D:\\dauvao250.txt", s250_2.dulieu250);
+	
+	Source500 s500_1, s500_2;
+	s500_1.LayMangFile500("D:\\dauvao500.txt",s500_1.dulieu500);
+	s500_2.LayMangFile500("D:\\dauvao500.txt",s500_2.dulieu500);
+	
+	MergeSort ms100, ms250, ms500;
+	HeapSort hs100, hs250, hs500;
+	
+	// hien thi
+	cout << "Size\t\t100\t250\t500";
+	cout << "\nMerge Sort\t" << ms100.t_MergeSort(s100_1.dulieu100,s100_1.n) << "\t" << ms250.t_MergeSort(s250_1.dulieu250,s250_1.n) << "\t" << ms500.t_MergeSort(s500_1.dulieu500,s500_1.n);
+	cout << "\nHeap Sort\t"  << hs100.t_HeapSort(s100_2.dulieu100,s100_2.n)  << "\t" << hs250.t_HeapSort(s250_2.dulieu250,s250_2.n)  << "\t" << hs500.t_HeapSort(s500_2.dulieu500,s500_2.n);
+	
+    return 0;
 }
