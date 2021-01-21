45ae34968161beccc173a5f2d9631f4a5e619251
Võ Đình Chí <chi.vd.60cntt@ntu.edu.vn>
2021-01-21 16:33:00 +0700

    cap nhat do hoa

--- CodeChinh.cpp
+++ CodeChinh.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-#include <fstream>
+#include <bits/stdc++.h> 
 #include <ctime>
+#include <cstdio>
+#include <fstream> 
+#include <time.h>
+#include <cstdlib>
+#include <string>
+#include <conio.h>
 #include <unistd.h>
+#include <graphics.h>
 
 using namespace std;
 class Source
@@ -97,11 +104,28 @@ class Source500: public Source
 /*====================================================================*/
 class Sort
 {
-	public: void inmang(int arr[],int n)
+	public:
+	void inmang(int arr[],int n)
     {
         for (int i = 0 ; i < n ; i++) 
         cout << arr[i] << " "; 
     }
+    void hinhchunhat(int x1, int y1, int x2, int y2)
+    {
+	    line(x1,y1,x1,y2);
+	    line(x1,y2,x2,y2);
+	    line(x2,y2,x2,y1);
+	    line(x2,y1,x1,y1);  
+    } 
+	//in ra man hinh day so theo do hoa
+	void screen(int arr[], int n)
+	{
+		int xstart = 0, ystart = 0;
+		for(int i = 0; i < n; i++)
+		{
+			hinhchunhat(xstart + i * 800 / n, 0, xstart + ( i + 1 ) * 800 / n,	arr[i] / 100);
+		}
+	}
 };
 class MergeSort: public Sort
 {	
@@ -161,6 +185,23 @@ class MergeSort: public Sort
 		    merge(arr, l, m, r);
 	    }
 	}
+	// ---- do hoa minh hoa thuat toan merge
+	void mergeSort_dohoa(int arr[], int l, int r)
+	{
+	    if(l<r)
+		{	
+			int m = ( l + r ) / 2;
+			// de qui tach mang
+		    mergeSort(arr, l, m);
+		    mergeSort(arr, m+1, r);
+		    // tron code 
+		    merge(arr, l, m, r);
+		    cleardevice();
+			Sort::screen(arr,r+1);
+			delay(100);
+	    }
+	}
+	//-----------------------------------------
 	void InMang(int arr[], int n)
 	{
 		Sort::inmang(arr, n);
@@ -215,6 +256,28 @@ class HeapSort : public Sort
 			heapify(arr, i, 0);
 		}
 	}
+	// ---Do hoa thuat toan Heap Sort ------
+	void heapSort_dohoa(int arr[], int n)
+	{
+		// Tao mot dong (Sap xep lai mang)
+		for (int i = n / 2 - 1; i >= 0; i--)
+		{
+			heapify(arr, n, i);
+			cleardevice();
+			Sort::screen(arr,n);
+			delay(100);
+		}
+		for (int i = n - 1; i >= 0; i--)
+		{
+			// Di chuyen root ve cuoi cung
+			swap(arr[0], arr[i]);
+			// goi ham heapify
+			heapify(arr, i, 0);
+			cleardevice();
+			Sort::screen(arr,n);
+			delay(100);
+		}
+	}
 	void InMang (int arr[], int n)
 	{
 		Sort::inmang(arr, n);
@@ -230,40 +293,59 @@ class HeapSort : public Sort
 		return d;
 	}
 };
+void laymang(Source100 &s100,Source250 &s250,Source500 &s500)
+{
+	s100.LayMangFile100("D:\\dauvao100.txt",s100.dulieu100);
+    s250.LayMangFile250("D:\\dauvao250.txt",s250.dulieu250);
+	s500.LayMangFile500("D:\\dauvao500.txt",s500.dulieu500);	
+}
 int main()
 {
-	Source100 s100_1, s100_2;
-	s100_1.LayMangFile100("D:\\dauvao100.txt", s100_1.dulieu100);
-	s100_2.LayMangFile100("D:\\dauvao100.txt", s100_2.dulieu100);
-	
-	Source250 s250_1, s250_2;
-	s250_1.LayMangFile250("D:\\dauvao250.txt", s250_1.dulieu250);
-	s250_2.LayMangFile250("D:\\dauvao250.txt", s250_2.dulieu250);
-	
-	Source500 s500_1, s500_2;
-	s500_1.LayMangFile500("D:\\dauvao500.txt",s500_1.dulieu500);
-	s500_2.LayMangFile500("D:\\dauvao500.txt",s500_2.dulieu500);
+	initwindow(800,1000);
+	Source100 s100;
+	Source250 s250;
+	Source500 s500;
 	
 	MergeSort ms100, ms250, ms500;
 	HeapSort hs100, hs250, hs500;
 	
-	// hien thi
 	double sumMS100=0, sumMS250=0, sumMS500=0;
 	double sumHS100=0, sumHS250=0, sumHS500=0;
+	
+	// tinh thoi gian trung binh sau 10 lan thuc hien
 	cout << "Size\t\t100\t250\t500";
 	for(int i= 0;i<10;i++)
 	{
-		//cout << "\nMerge Sort\t" << ms100.t_MergeSort(s100_1.dulieu100,s100_1.n) << "\t" << ms250.t_MergeSort(s250_1.dulieu250,s250_1.n) << "\t" << ms500.t_MergeSort(s500_1.dulieu500,s500_1.n);
-		//cout << "\nHeap Sort\t"  << hs100.t_HeapSort(s100_2.dulieu100,s100_2.n)  << "\t" << hs250.t_HeapSort(s250_2.dulieu250,s250_2.n)  << "\t" << hs500.t_HeapSort(s500_2.dulieu500,s500_2.n);
-    	sumMS100 = sumMS100 + ms100.t_MergeSort(s100_1.dulieu100,s100_1.n);
-		sumMS250 = sumMS250 + ms250.t_MergeSort(s250_1.dulieu250,s250_1.n);
-		sumMS500 = sumMS500 + ms500.t_MergeSort(s500_1.dulieu500,s500_1.n);
+		sumMS100 = sumMS100 + ms100.t_MergeSort(s100.dulieu100,s100.n);
+		sumMS250 = sumMS250 + ms250.t_MergeSort(s250.dulieu250,s250.n);
+		sumMS500 = sumMS500 + ms500.t_MergeSort(s500.dulieu500,s500.n);
 		
-		sumHS100 = sumHS100 + hs100.t_HeapSort(s100_2.dulieu100,s100_2.n);
-		sumHS250 = sumHS250 + hs250.t_HeapSort(s250_2.dulieu250,s250_2.n);
-		sumHS500 = sumHS500 + hs500.t_HeapSort(s500_2.dulieu500,s500_2.n);
+		sumHS100 = sumHS100 + hs100.t_HeapSort(s100.dulieu100,s100.n);
+		sumHS250 = sumHS250 + hs250.t_HeapSort(s250.dulieu250,s250.n);
+		sumHS500 = sumHS500 + hs500.t_HeapSort(s500.dulieu500,s500.n);
 	}
 	cout << "\nMerge Sort :\t" << sumMS100/10 << "\t" << sumMS250/10 << "\t" << sumMS500/10;
 	cout << "\nHeap Sort :\t" << sumHS100/10 << "\t" << sumHS250/10 << "\t" << sumHS500/10;
-	return 0;
+	
+	// bieu dien do hoa
+	int lc;
+	cout << "\n\nDo hoa minh hoa:\n(1: Do hoa Merge Sort| 2: Do hoa Heap Sort)" << endl;
+   	cout << ">>";	cin >> lc;
+   	if(lc == 1)
+   	{	// minh hoa qua trinh sap xep voi dau vao 100
+   		laymang(s100,s250,s500);
+   		ms100.mergeSort_dohoa(s100.dulieu100,0,s100.n-1);
+ 		//ms250.heapSort_dohoa(s250.dulieu250,0,s250.n-1);
+ 		//ms500.heapSort_dohoa(s500.dulieu500,0,s250.n-1);
+   	}
+   	if(lc == 2)
+   	{	// minh hoa qua trinh sap xep voi dau vao 100
+   		laymang(s100,s250,s500);
+   		hs100.heapSort_dohoa(s100.dulieu100,s100.n);
+ 		//hs250.heapSort_dohoa(s250_2.dulieu250,s250_2.n);
+ 		//hs500.heapSort_dohoa(s500_2.dulieu500,s250_2.n);
+   	}
+  	fflush(stdin);
+	getchar();
+  	system("cls");
 }
